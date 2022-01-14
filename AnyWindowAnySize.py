4c120fa77eede3738e9dd4878f94eb78ccf0a010
samofhearts <slpeters500@gmail.com>
2022-01-14 14:58:13 -0700

    Initial commit

---
+++ AnyWindowAnySize.py
@@ -0,0 +1,144 @@
+from turtle import seth
+import pygetwindow
+import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
+import ctypes
+from ctypes import wintypes
+
+
+## Global Variables
+processList = []
+optionList = []
+
+def applyIt():
+    global clicked
+    global heightEntry
+    global widthEntry
+    global processList
+    global optionList
+    selectedWindow = clicked.get()
+    setHeight = heightEntry.get()
+    setWidth = widthEntry.get()
+
+    if (len(pygetwindow.getWindowsWithTitle(selectedWindow))) > 1:
+        for each in processList:
+            if each.title == str(selectedWindow):
+                for wind in pygetwindow.getWindowsWithTitle(selectedWindow):
+                    if wind.left == each.left and wind.top == each.top and wind.width == each.width and wind.height == each.height and wind.title == each.title:
+                        wind.size = (int(setWidth), int(setHeight))
+    else:
+        win = pygetwindow.getWindowsWithTitle(selectedWindow)[0]
+        win.size = (int(setWidth), int(setHeight))
+
+def minusOnePercent():
+    global clicked
+    global heightEntry
+    global widthEntry
+    global processList
+    global optionList
+    selectedWindow = clicked.get()
+
+    if (len(pygetwindow.getWindowsWithTitle(selectedWindow))) > 1:
+        for each in processList:
+            if each.title == str(selectedWindow):
+                for wind in pygetwindow.getWindowsWithTitle(selectedWindow):
+                    if wind.left == each.left and wind.top == each.top and wind.width == each.width and wind.height == each.height and wind.title == each.title:
+                        wind.size = (int(wind.width - (wind.width * 0.01)), int(wind.height - (wind.height * 0.01)))
+    else:
+        win = pygetwindow.getWindowsWithTitle(selectedWindow)[0]
+        win.size = (int(win.width - (win.width * 0.01)), int(win.height - (win.height * 0.01)))
+
+def plusOnePercent():
+    global clicked
+    global heightEntry
+    global widthEntry
+    global processList
+    global optionList
+    selectedWindow = clicked.get()
+
+    if (len(pygetwindow.getWindowsWithTitle(selectedWindow))) > 1:
+        for each in processList:
+            if each.title == str(selectedWindow):
+                for wind in pygetwindow.getWindowsWithTitle(selectedWindow):
+                    if wind.left == each.left and wind.top == each.top and wind.width == each.width and wind.height == each.height and wind.title == each.title:
+                        wind.size = (int(wind.width + (wind.width * 0.01)), int(wind.height + (wind.height * 0.01)))
+    else:
+        win = pygetwindow.getWindowsWithTitle(selectedWindow)[0]
+        win.size = (int(win.width + (win.width * 0.01)), int(win.height + (win.height * 0.01)))
+
+
+## Get Process Options
+##print(pygetwindow.getAllWindows())
+
+processList = pygetwindow.getAllWindows()
+
+for each in processList:
+    ##print(each)
+    if(len(str(each.title)) < 1):
+        pass
+    else:
+        optionList.append(each.title)
+
+## Make GUI Window
+master = Tk()
+master.title('Any Window Any Size')
+master.geometry('500x350')
+
+## Set Background Stuff
+bgCanvas = Canvas(master,bg="black",width=500, height=350)
+background_image = Image.open('background.png')
+tkImage = ImageTk.PhotoImage(background_image)
+background_label = Label(master, image=tkImage)
+background_label.place(x=0,y=0, relwidth=1, relheight = 1)
+
+## Blank Rows / buffers
+master.grid_rowconfigure(0,minsize=50)
+master.grid_rowconfigure(2,minsize=20)
+master.grid_rowconfigure(5,minsize=5)
+master.grid_rowconfigure(8,minsize=5)
+master.grid_rowconfigure(11,minsize=5)
+master.grid_rowconfigure(13,minsize=10)
+
+## Blank Columns / buffers
+master.grid_columnconfigure(0,minsize=100)
+master.grid_columnconfigure(1,minsize=5)
+master.grid_columnconfigure(2,minsize=5)
+master.grid_columnconfigure(3,minsize=5)
+master.grid_columnconfigure(4,minsize=100)
+master.grid_columnconfigure(2,weight=1)
+
+## Title Area
+titleText = Label(master, bg='white', text="Any Window Any Size", font=("Times New Roman",16)).grid(row=1, columnspan=5)
+
+## Select Window Dropdown
+clicked = StringVar()
+
+windowSelectText = Label(master, bg='white', text="Select Window", font=("Times New Roman",16)).grid(row=3, columnspan=5)
+drop = OptionMenu(master, clicked, *optionList)
+drop.grid(row=4, column=2)
+drop.config(width=40)
+
+## Width Input Box
+
+widthText = Label(master, bg='white', text="Width", font=("Times New Roman",16)).grid(row=6, column=2, sticky=W)
+widthEntry = Entry(master, bg='white', width=15, highlightthickness=3)
+widthEntry.grid(row=7,column=2, sticky=W)
+
+## Height Input Box
+heightText = Label(master, bg='white', text="Height", font=("Times New Roman",16)).grid(row=6, column=2, sticky=E)
+heightEntry = Entry(master, bg='white', width=15, highlightthickness=3)
+heightEntry.grid(row=7,column=2, sticky=E)
+
+## -1% Button
+minusPercentButton = Button(master, bg='white', text="Minus 1 Percent", command = minusOnePercent).grid(row=12, column=2, sticky=W)
+
+## +1% Button
+plusPercentButton = Button(master, bg='white', text="Plus 1 Percent", command = plusOnePercent).grid(row=12, column=2, sticky=E)
+
+## Apply Button
+applyButton = Button(master, bg='white', text="Apply",command = applyIt).grid(row=15, columnspan=5)
+
+
+## Mainloop keeps tkinter window open
+master.mainloop()
---
+++ README.md
@@ -0,0 +1,2 @@
+# anywindowanysize
+ Resize any window in Windows to any size you wish (width and height in pixels)
